339e1e0731e7d037234e2f98d3cac1aa88973698
Markus Hackspacher <hackspacher@gmx.de>
2024-09-01 21:18:59 +0200

    test gui

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -6,4 +6,18 @@ from main import app # Flask instance of the API
 def test_index_route():
     response = app.test_client().get('/')
 
-    assert response.status_code == 404
+    assert response.status_code == 200
+    assert response.data.decode('utf-8')[:15] == '<!DOCTYPE html>'
+
+def test_second_route():
+    response = app.test_client().get('/second_gui')
+
+    assert response.status_code == 500
+    assert response.data.decode('utf-8')[:15] == '<!doctype html>'
+
+
+def test_third_route():
+    response = app.test_client().get('/third_gui')
+
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Keine Daten ausgewählt.'
